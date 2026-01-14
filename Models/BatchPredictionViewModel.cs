5391705f0f1e28892b9a2446a977e8f8db906d41
Flaviu <pauliepana92@gmail.com>
2026-01-14 21:11:22 +0200

    Batch Prediction

---
+++ Models/BatchPredictionViewModel.cs
@@ -0,0 +1,8 @@
+﻿using static Iepan_Flaviu_Lab4.PricePredictionModel;
+public class BatchPredictionViewModel
+{
+    public IFormFile? File { get; set; }
+    // Rezultatele (predicțiile) pentru fiecare rând din fișier
+    public List<ModelOutput>? Predictions { get; set; }
+    public string? ErrorMessage { get; set; }
+}
